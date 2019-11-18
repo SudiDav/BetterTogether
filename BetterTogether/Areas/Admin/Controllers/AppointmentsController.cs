80e5818b4b2fb78db0607501be31897e96c4be61
Sudi David <naohdvd1990@gmail.com>
2019-11-19 01:17:54 +0200

    Modefied the AppointmentController

--- BetterTogether/Areas/Admin/Controllers/AppointmentsController.cs
+++ BetterTogether/Areas/Admin/Controllers/AppointmentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BetterTogether.Data;
 using BetterTogether.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,11 @@ namespace BetterTogether.Areas.Admin.Controllers
     [Area("Admin")]
     public class AppointmentsController : Controller
     {
-
+        private readonly ApplicationDbContext _context;
+        public AppointmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
