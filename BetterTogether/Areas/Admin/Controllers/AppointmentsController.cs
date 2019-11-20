caaf909e4eaa2e3012baba1da485fe27dfab51d7
Sudi David <naohdvd1990@gmail.com>
2019-11-20 16:37:40 +0200

    AddedAppointmentControllerAndView

--- BetterTogether/Areas/Admin/Controllers/AppointmentsController.cs
+++ BetterTogether/Areas/Admin/Controllers/AppointmentsController.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using BetterTogether.Data;
+using BetterTogether.Models.ViewModels;
 using BetterTogether.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BetterTogether.Areas.Admin.Controllers
 {
@@ -14,13 +18,101 @@ namespace BetterTogether.Areas.Admin.Controllers
     public class AppointmentsController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private int PageSize = 3;
+
         public AppointmentsController(ApplicationDbContext context)
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            System.Security.Claims.ClaimsPrincipal currentUser = this.User;
+            var claimsIdentity = (ClaimsIdentity)this.User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            AppointmentViewModel appointmentViewModel = new AppointmentViewModel()
+            {
+                Appointments = new List<Models.Appointments>()
+            };
+
+            StringBuilder param = new StringBuilder();
+
+            param.Append("/Admin/Appointments?productPage=:");
+            param.Append("&searchName=");
+            //if (searchName != null)
+            //{
+            //    param.Append(searchName);
+            //}
+            //param.Append("&searchEmail=");
+            //if (searchEmail != null)
+            //{
+            //    param.Append(searchEmail);
+            //}
+            //param.Append("&searchPhone=");
+            //if (searchPhone != null)
+            //{
+            //    param.Append(searchPhone);
+            //}
+            //param.Append("&searchDate=");
+            //if (searchDate != null)
+            //{
+            //    param.Append(searchDate);
+            //}
+
+
+
+
+            appointmentViewModel.Appointments = _context.Appointments.Include(a => a.SalesPerson).ToList();
+            if (User.IsInRole(StaticDetails.AdminUser))
+            {
+                appointmentViewModel.Appointments = appointmentViewModel.Appointments.Where(a => a.SalesPersonId == claim.Value).ToList();
+            }
+
+
+            //if (searchName != null)
+            //{
+            //    appointmentViewModel.Appointments = appointmentViewModel.Appointments.Where(a => a.CustName.ToLower().Contains(searchName.ToLower())).ToList();
+            //}
+            //if (searchEmail != null)
+            //{
+            //    appointmentViewModel.Appointments = appointmentViewModel.Appointments.Where(a => a.CustEmail.ToLower().Contains(searchEmail.ToLower())).ToList();
+            //}
+            //if (searchPhone != null)
+            //{
+            //    appointmentViewModel.Appointments = appointmentViewModel.Appointments.Where(a => a.CustPhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToList();
+            //}
+            //if (searchDate != null)
+            //{
+            //    try
+            //    {
+            //        DateTime appDate = Convert.ToDateTime(searchDate);
+            //        appointmentViewModel.Appointments = appointmentViewModel.Appointments.Where(a => a.AppointmentDate.ToShortDateString().Equals(appDate.ToShortDateString())).ToList();
+            //    }
+            //    catch (Exception ex)
+            //    {
+
+            //    }
+
+            //}
+
+            var count = appointmentViewModel.Appointments.Count;
+
+            //appointmentViewModel.Appointments = appointmentViewModel.Appointments.OrderBy(p => p.AppointmentDate)
+            //    .Skip((productPage - 1) * PageSize)
+            //    .Take(PageSize).ToList();
+
+
+            //appointmentViewModel.PagingInfo = new PagingInfo
+            //{
+            //    CurrentPage = productPage,
+            //    ItemsPerPage = PageSize,
+            //    TotalItems = count,
+            //    urlParam = param.ToString()
+            //};
+
+
+            return View(appointmentViewModel);
+           
         }
     }
 }
